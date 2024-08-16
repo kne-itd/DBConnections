ef6be7152f984b47aa82a65b05c704ef6ce56548
kne-itd <kne@sde.dk>
2024-08-16 10:43:12 +0200

    Added test for current network

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using System.Data.SqlClient;
+using System.Net;
 /*
  * https://www.connectionstrings.com/microsoft-data-sqlclient/
  */
@@ -10,16 +11,29 @@ namespace SQLserverConnection
     {
         public static void Main(string[] args)
         {
-
-            //string ConnectionString = "Server=172.16.226.2;" +
-            //    "Database=AdventureWorks2017;" +
-            //    "Uid=Mac;" +
-            //    "Pwd = 1234; ";
-
-            string ConnectionString = "Server=kajs-MacBook-Pro.local;" +
+            string HomeIP = "172.16.226.2";
+            string WorkIp = "10.130.64.202";
+            string DbServerAddress = string.Empty;
+            string hostName = Dns.GetHostName(); ;
+            string CurrentIp = Dns.GetHostEntry(hostName).AddressList[1].ToString();
+            //string CurrentIp = Dns.GetHostByName(hostName).AddressList[1].ToString();
+            if (CurrentIp.StartsWith("172."))
+            {
+                DbServerAddress = HomeIP;
+            }
+            else if (CurrentIp.StartsWith("10."))
+            {
+                 DbServerAddress=WorkIp;
+            }
+            string ConnectionString = $"Server={DbServerAddress};" +
                 "Database=AdventureWorks2017;" +
                 "Uid=Mac;" +
-                "Pwd = 1234;";
+                "Pwd = 1234; ";
+            Console.WriteLine(ConnectionString);
+            //string ConnectionString = "Server=Kajs-MacBook-Pro.local;" +
+            //    "Database=AdventureWorks2017;" +
+            //    "Uid=Mac;" +
+            //    "Pwd = 1234;";
 
 
             string Query = "select * from Person.PhoneNumberType";
